8179ecc308c79207f2aa0aa6c6ff7aafef41a6b1
X3z4YkM <X3z4YkM@gmail.com>
2022-08-24 13:22:16 -0700

    update

---
+++ build/src/Descriptor.d
@@ -0,0 +1,6 @@
+build/src/Descriptor.o: src/Descriptor.cpp src/../h/Descriptor.hpp \
+ src/../h/../lib/hw.h
+
+src/../h/Descriptor.hpp:
+
+src/../h/../lib/hw.h:
---
+++ build/src/MemAlloc.d
@@ -0,0 +1,8 @@
+build/src/MemAlloc.o: src/MemAlloc.cpp src/../h/MemAlloc.hpp \
+ src/../h/../lib/hw.h src/../h/../h/Descriptor.hpp
+
+src/../h/MemAlloc.hpp:
+
+src/../h/../lib/hw.h:
+
+src/../h/../h/Descriptor.hpp:
